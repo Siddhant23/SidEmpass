b7517658cb103792299e00b15c935321a51088af
siddhant <s.mehta@huulke.com>
2018-11-21 23:15:26 +0530

    INitial Commit

---
+++ app/src/main/java/com/sid/MainActivity.java
@@ -0,0 +1,25 @@
+package com.sid;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnOpenLib).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
+
+    }
+}
+
